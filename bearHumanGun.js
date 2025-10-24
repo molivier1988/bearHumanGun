11e662820c7c448556e4c3d38775fd4ce79b1071
molivier1988 <markolivier1969@gmail.com>
2025-10-24 13:04:05 +0100

    updated bearHumanGun.js

--- bearHumanGun.js
+++ bearHumanGun.js
@@ -1,82 +1,77 @@
-// Function to get user game choice and check for valid input
+// create function getUserChoice with a parameter of `userInput`
 function getUserChoice(userInput) {
-  // Convert input to lower case
-  userInput = userInput.toLowerCase();
-  // Tests for valid user input. If valid user choice is returned else
-  // error message is returned
-  if (userInput === "bear" || userInput === "human" || userInput === "gun") {
-    return console.log(userInput);
-  } else {
-    console.log("Sorry your input is invalid");
-  }
+    // turn future `userInput` to lower case
+    userInput = userInput.toLowerCase();
+    // if there is a match return `userInput`
+    if (userInput === 'bear' || userInput === 'human' || userInput === 'gun') {
+        return userInput;
+      // if no match prompt user for valid entry
+    } else {
+        return 'Please enter valid option';
+    }
 }
 
-// Function to randomly select computer choice
+// create a function that will provide a random computer choice for each round
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  console.log(randomNumber);
-  if (randomNumber === 0) {
-    return "bear";
-  } else if (randomNumber === 1) {
-    return "human";
-  } else {
-    return "gun";
-  }
+    var randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return 'bear';
+    } else if (randomNumber === 1) {
+        return 'human';
+    } else {
+        return 'gun';
+    }
 }
-
-// Function to determine game winner by comparing
-// userChoice and computerChoice inputs
+// create a function `determineWinner` and pass in parameters
+// `userChoice` and `computerChoice`
 function determineWinner(userChoice, computerChoice) {
-  // if there is a tie
-  if (userChoice === computerChoice) {
-    return "It is a tie";
-  }
-  // if user selects `'human'`
-  if (userChoice === "human") {
-    // if computer selects `'bear'`
-    if (computerChoice === "bear") {
-      return "You have been mauled by a bear";
-    } else {
-      return "You have disarmed a gun";
+    // if there is a tie
+    if (userChoice === computerChoice) {
+        return 'It is a tie';
     }
-  }
-  // if user selects `'bear'`
-  if (userChoice === "bear") {
-    // if computer selects `'gun'`
-    if (computerChoice === "gun") {
-      return "You have been shot by a gun";
-    } else {
-      return "You have mauled a human";
+    // if user selects `'human'`
+    if (userChoice === 'human') {
+        // if computer selects `'bear'`
+        if (computerChoice === 'bear') {
+            return 'You have been mauled by a bear';
+        } else {
+            return 'You have disarmed a gun';
+        }
     }
-  }
-  // if user selects `'gun'`
-  if (userChoice === "gun") {
-    // if computer selectsion `'human'`
-    if (computerChoice === "human") {
-      return "Your gun has been disarmed";
-    } else {
-      return "You have shot a bear";
+    // if user selects `'bear'`
+    if (userChoice === 'bear') {
+        // if computer selects `'gun'`
+        if (computerChoice === 'gun') {
+            return 'You have been shot by a gun';
+        } else {
+            return 'You have mauled a human';
+        }
     }
-  }
+    // if user selects `'gun'`
+    if (userChoice === 'gun') {
+        // if computer selectsion `'human'`
+        if (computerChoice === 'human') {
+            return 'Your gun has been disarmed';
+        } else {
+            return 'You have shot a bear';
+        }
+    }
 }
 
-// Function to start game, calls functions to:
-// 1. Get user input
-// 2. Check if user input is valid and print to the console
-// 3. Get random computer choice and print to the console
-// 4. Determine who wins the game
+
 function playGame() {
-  // prompt user to make choice
-  var promptUsesChoice = prompt("Please choose bear, human or gun");
-  // format user choice to lower case and check for valid word
-  var userChoice = getUserChoice(promptUsesChoice);
-  // call `getComputerChoice()` and assign to variable `computerChoice`
-  var computerChoice = getComputerChoice();
-  console.log(computerChoice);
-  console.log(userChoice);
-  // pass `userChoice` and `computerChoice` to the function `determineWinner`
-  // and log to the console
-  console.log(determineWinner(userChoice, computerChoice));
+    // prompt user to make choice
+    var promptUsesChoice = prompt("Please choose bear, human or gun");
+    // format user choice to lower case and check for valid word
+    var userChoice = getUserChoice(promptUsesChoice);
+    // call `getComputerChoice()` and assign to variable `computerChoice` 
+    var computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    console.log(userChoice);
+    // pass `userChoice` and `computerChoice` to the function `determineWinner`
+    // and log to the console
+    console.log(determineWinner(userChoice, computerChoice));
 }
 
-playGame(); // Call playGame()
+// call `playGame` function
+playGame();
